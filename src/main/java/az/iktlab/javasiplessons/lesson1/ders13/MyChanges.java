54b9805878fe73c5d5cfe478177cfc7203afb190
Mehmet Elibeyli <tomristt9@gmail.com>
2023-07-26 20:36:07 +0400

    adding mychanges class

---
+++ src/main/java/az/iktlab/javasiplessons/lesson1/ders13/MyChanges.java
@@ -0,0 +1,7 @@
+package az.iktlab.javasiplessons.lesson1.ders13;
+
+public class MyChanges {
+    public static void main(String[] args) {
+        System.out.println("I made some changes");
+    }
+}
